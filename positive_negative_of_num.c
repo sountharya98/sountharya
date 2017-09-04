4d479ee61c92b10d3af9c2a480927436c29385c6
sountharya98 <31615521+sountharya98@users.noreply.github.com>
2017-09-04 18:20:55 +0530

    Create positive_negative_of_num.c

---
+++ positive_negative_of_num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter the no");
+scanf("%d",&num);
+if(num>0)
+printf("positive no");
+else if(num<0)
+printf("negative no");
+else
+printf("zero");
+}
